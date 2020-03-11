e1420c8d205cd9cc157e22a616c6cca8c2f79e0f
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-11 11:38:16 +0100

    Created Vector2 object

---
+++ src/Engine/vector2.cc
@@ -0,0 +1,34 @@
+#include "../lib/Engine/vector2.hh"
+
+Vector2::Vector2() { set(0, 0); }
+Vector2::Vector2(const float x, const float y) { set(x, y); }
+
+Vector2& Vector2::set(const float x, const float y)
+{ this->x = x; this->y = y; return *this; }
+
+Vector2& Vector2::scale(const float scalar)
+{ this->x *= scalar; this->y *= scalar; return *this; }
+
+Vector2& Vector2::add(const Vector2& vec)
+{ this->x += vec.x; this->y += vec.y; return *this; }
+
+Vector2& Vector2::sub(const Vector2& vec)
+{ this->x -= vec.x; this->y -= vec.y; return *this; }
+
+Vector2& Vector2::mul(const Vector2& vec)
+{ this->x *= vec.x; this->y *= vec.y; return *this; }
+
+Vector2& Vector2::div(const Vector2& vec)
+{ this->x /= vec.x; this->y /= vec.y; return *this; }
+
+Vector2& operator+(Vector2& v1, const Vector2& v2) { return v1.add(v2); }
+Vector2& operator-(Vector2& v1, const Vector2& v2) { return v1.sub(v2); }
+Vector2& operator*(Vector2& v1, const Vector2& v2) { return v1.mul(v2); }
+Vector2& operator/(Vector2& v1, const Vector2& v2) { return v1.div(v2); }
+
+Vector2& Vector2::operator+=(const Vector2& vec) { return this->add(vec); }
+Vector2& Vector2::operator+=(const Vector2& vec) { return this->sub(vec); }
+Vector2& Vector2::operator+=(const Vector2& vec) { return this->mul(vec); }
+Vector2& Vector2::operator+=(const Vector2& vec) { return this->div(vec); }
+
+Vector2& Vector2::operator^(const float& s) { return this->scale(s); }
