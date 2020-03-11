561121771509a8c2baef319c2b1bfe85cc8ec2bb
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-11 11:18:20 +0100

    Started on components

---
+++ src/lib/ECS Components/components.hh
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "../Engine/ecs.hh"
+#include "transform.hh"
---
+++ src/lib/ECS Components/transform.hh
@@ -0,0 +1,7 @@
+#pragma once
+#include "../Engine/ecs.hh"
+
+class TransformComponent : public Component
+{
+
+};
