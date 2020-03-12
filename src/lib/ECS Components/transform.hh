045f4901afe3119b70bbbb38769d70c2b64f0b42
BitTim <tim.anhalt.2004@protonmail.com>
2020-03-12 16:29:48 +0100

    Updated transform

--- src/lib/ECS Components/transform.hh
+++ src/lib/ECS Components/transform.hh
@@ -2,17 +2,18 @@
 
 #include "../Engine/ecs.hh"
 #include "../Engine/vector2.hh"
+#include "../Engine/constants.hh"
 
 class TransformComponent : public Component
 {
-private:
+public:
   Vector2 pos;
   Vector2 size;
+  int scale = 1;
 
-public:
   TransformComponent() { pos.set(0, 0); }
   TransformComponent(int x, int y, int scale)
-  { pos.set(x, y); size.set(32, 32); size.scale(scale); }
+  { pos.set(x, y); size.set(_TILESIZE, _TILESIZE); this->scale = scale; }
   TransformComponent(int x, int y, int w, int h, int scale)
-  { pos.set(x, y); size.set(32, 32); size.scale(scale); }
+  { pos.set(x, y); size.set(w, h); this->scale = scale; }
 };
